e8a4668691d96699bdbfe5d54a0d5d917c63e0ca
Sapir Twig <110773559+sapirtwig@users.noreply.github.com>
2025-06-04 10:47:07 +0300

    Delete Not-Secured directory

--- Not-Secured/backend/configuration.py
+++
@@ -1,9 +0,0 @@
-# Email configuration
-smtp_server = "smtp.gmail.com"
-smtp_port = 587
-sender_email = "projectnetworking573@gmail.com"
-sender_password = "tywf tnnf xcew xjnb"
-subject = "Your Recovery Password"
-
-
-
